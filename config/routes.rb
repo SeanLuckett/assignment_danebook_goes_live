0163ddd7ed3922f173b3e688079216662a7b00b3
Sean Luckett <whitecrane007@gmail.com>
2017-08-08 09:58:06 -0600

    Root route goes to static_pages#home; creates StaticPages controller as well as static views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'static_pages/home'
+
+  get 'static_pages/timeline'
+
+  get 'static_pages/about'
+
+  get 'static_pages/friends'
+
+  get 'static_pages/photos'
+
+  root to: 'static_pages#home'
 end
