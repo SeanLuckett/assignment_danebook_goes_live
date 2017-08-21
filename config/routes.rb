690b86a28f18f76c83c244e43334f410278fd88b
Sean Luckett <whitecrane007@gmail.com>
2017-08-21 11:14:28 -0600

    Complete photos page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
   get '/timeline', to: 'static_pages#timeline'
 
   get '/about', to: 'static_pages#about'
 
   get '/friends', to: 'static_pages#friends'
 
-  get 'static_pages/photos'
+  get '/photos', to: 'static_pages#photos'
 
   root to: 'static_pages#home'
 end
